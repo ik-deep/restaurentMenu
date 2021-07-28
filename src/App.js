da43ee30f98bc35b0fec6399a8f12c9312436898
Irfan <ik7417990@gmail.com>
2021-07-28 21:06:48 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import  Restaurent  from './components/basics/Restaurent'
+
+const App = () => {
+  return (
+    <div>
+      <Restaurent/>
+    </div>
+  )
+}
+
+export default App
